8cd1791d72dc583af52a49dfb3be3408216db815
soonhankwon <soonable@gmail.com>
2023-07-18 13:55:15 +0900

    config : interceptors & cores config

---
+++ src/main/java/soon/devspacexbackend/config/WebConfig.java
@@ -0,0 +1,25 @@
+package soon.devspacexbackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import soon.devspacexbackend.web.interceptor.LoginCheckInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/users/signup", "/login", "/logout",
+                        "/css/**","/js/**", "/*.ico", "/error", "/swagger-ui/**", "/v3/api-docs/**");
+    }
+}
