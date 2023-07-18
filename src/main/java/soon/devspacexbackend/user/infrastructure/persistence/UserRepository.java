c05b8b5b6adf8f3fb96f8d0ca7f9ef9bb92d3364
soonhankwon <soonable@gmail.com>
2023-07-18 13:53:17 +0900

    feat : @Repository

--- src/main/java/soon/devspacexbackend/user/infrastructure/persistence/UserRepository.java
+++ src/main/java/soon/devspacexbackend/user/infrastructure/persistence/UserRepository.java
@@ -1,7 +1,12 @@
 package soon.devspacexbackend.user.infrastructure.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import soon.devspacexbackend.user.domain.User;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByEmailAndPassword(String email, String password);
 }
