c17195fbe5548f5c88481c2e41a6a6758781f987
soonhankwon <soonable@gmail.com>
2023-07-20 12:29:40 +0900

    feat : 컨텐츠 등록 응답 DTO

---
+++ src/main/java/soon/devspacexbackend/content/presentation/dto/ContentRegisterResDto.java
@@ -0,0 +1,9 @@
+package soon.devspacexbackend.content.presentation.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public final class ContentRegisterResDto {
+
+    private final String message = "컨텐츠 등록 완료";
+}
