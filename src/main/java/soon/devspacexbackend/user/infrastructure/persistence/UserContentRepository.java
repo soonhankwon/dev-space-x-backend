cd298fadc0df336ce2ccf42b19d8ca4c73c5b115
soonhankwon <soonable@gmail.com>
2023-07-21 15:05:10 +0900

    feat : 유저 컨텐츠 레포지토리

--- src/main/java/soon/devspacexbackend/user/infrastructure/persistence/UserContentRepository.java
+++ src/main/java/soon/devspacexbackend/user/infrastructure/persistence/UserContentRepository.java
@@ -2,8 +2,16 @@ package soon.devspacexbackend.user.infrastructure.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import soon.devspacexbackend.content.domain.Content;
+import soon.devspacexbackend.user.domain.BehaviorType;
+import soon.devspacexbackend.user.domain.User;
 import soon.devspacexbackend.user.domain.UserContent;
+
+import java.util.Optional;
 
 @Repository
 public interface UserContentRepository extends JpaRepository<UserContent, Long> {
+
+    boolean existsUserContentByContentAndUserAndType(Content content, User user, BehaviorType type);
+    Optional<UserContent> findUserContentByContentAndUserAndType(Content content, User user, BehaviorType type);
 }
