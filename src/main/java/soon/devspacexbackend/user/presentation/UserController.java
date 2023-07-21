ff5f40206a4207ec5d8587697e142f289d02e3c1
soonhankwon <soonable@gmail.com>
2023-07-21 18:36:22 +0900

    feat : 세션 서비스 적용으로 HttpSession 에 대한 결합도 개선, 특정 컨텐츠 사용자 이력 조회 API

--- src/main/java/soon/devspacexbackend/user/presentation/UserController.java
+++ src/main/java/soon/devspacexbackend/user/presentation/UserController.java
@@ -3,19 +3,15 @@ package soon.devspacexbackend.user.presentation;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import soon.devspacexbackend.user.application.UserService;
 import soon.devspacexbackend.user.domain.User;
-import soon.devspacexbackend.user.presentation.dto.UserResignReqDto;
-import soon.devspacexbackend.user.presentation.dto.UserResignResDto;
-import soon.devspacexbackend.user.presentation.dto.UserSignupReqDto;
-import soon.devspacexbackend.user.presentation.dto.UserSignupResDto;
-import soon.devspacexbackend.web.session.SessionConst;
+import soon.devspacexbackend.user.presentation.dto.*;
+import soon.devspacexbackend.web.application.SessionService;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import java.util.List;
 
 
 @RestController
@@ -25,6 +21,7 @@ import javax.servlet.http.HttpSession;
 public class UserController {
 
     private final UserService userServiceImpl;
+    private final SessionService sessionServiceImpl;
 
     @PostMapping("/signup")
     @Operation(summary = "회원 가입 API")
@@ -38,11 +35,16 @@ public class UserController {
     @Operation(summary = "회원 탈퇴 API")
     @ResponseStatus(HttpStatus.OK)
     public UserResignResDto resignUser(@RequestBody UserResignReqDto dto, HttpServletRequest request) {
-        HttpSession session = request.getSession(false);
-        User loginUser = (User) session.getAttribute(SessionConst.LOGIN_USER);
-
+        User loginUser = sessionServiceImpl.getLoginUserBySession(request);
         userServiceImpl.resignUser(dto, loginUser);
-        session.invalidate();
+        sessionServiceImpl.expireSession(request);
         return new UserResignResDto();
+    }
+
+    @GetMapping("/{contentId}/history")
+    @Operation(summary = "특정 컨텐츠 사용자 이력 조회 API")
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserHistoryGetContentResDto> getUsersHistoryByContent(@PathVariable Long contentId, HttpServletRequest request) {
+        return userServiceImpl.getUsersHistoryByContent(contentId);
     }
 }
