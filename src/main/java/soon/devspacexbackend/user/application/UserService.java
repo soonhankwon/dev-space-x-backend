16f64f6d145b69468baa7d430a970793bf4ec125
soonhankwon <soonable@gmail.com>
2023-07-21 18:37:56 +0900

    feat : 특정 컨텐츠 유저 조회 이력 조회 메서드

--- src/main/java/soon/devspacexbackend/user/application/UserService.java
+++ src/main/java/soon/devspacexbackend/user/application/UserService.java
@@ -1,11 +1,17 @@
 package soon.devspacexbackend.user.application;
 
 import soon.devspacexbackend.user.domain.User;
+import soon.devspacexbackend.user.presentation.dto.UserHistoryGetContentResDto;
 import soon.devspacexbackend.user.presentation.dto.UserResignReqDto;
 import soon.devspacexbackend.user.presentation.dto.UserSignupReqDto;
+
+import java.util.List;
 
 public interface UserService {
 
     void signupUser(UserSignupReqDto dto);
+
     void resignUser(UserResignReqDto dto, User loginUser);
+
+    List<UserHistoryGetContentResDto> getUsersHistoryByContent(Long contentId);
 }
