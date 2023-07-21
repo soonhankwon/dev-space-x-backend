d751b54ad24578f57232d624d8349f8de47ce2e4
soonhankwon <soonable@gmail.com>
2023-07-21 11:55:34 +0900

    feat : 회원 탈퇴 기능 추상화

--- src/main/java/soon/devspacexbackend/user/application/UserService.java
+++ src/main/java/soon/devspacexbackend/user/application/UserService.java
@@ -1,9 +1,11 @@
 package soon.devspacexbackend.user.application;
 
+import soon.devspacexbackend.user.domain.User;
+import soon.devspacexbackend.user.presentation.dto.UserResignReqDto;
 import soon.devspacexbackend.user.presentation.dto.UserSignupReqDto;
 
 public interface UserService {
 
     void signupUser(UserSignupReqDto dto);
-
+    void resignUser(UserResignReqDto dto, User loginUser);
 }
