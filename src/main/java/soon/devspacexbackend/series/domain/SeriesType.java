a1ef69706b7bbc45dc23231ffe64122af7963412
soonhankwon <soonable@gmail.com>
2023-07-21 11:53:17 +0900

    feat : 시리즈 타입 Enum Class 월,화,수,목,금,토,일,제한없음

---
+++ src/main/java/soon/devspacexbackend/series/domain/SeriesType.java
@@ -0,0 +1,8 @@
+package soon.devspacexbackend.series.domain;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum SeriesType {
+    SUN, MON, TUE, WED, THU, FRI, SAT, NO_LIMIT;
+}
