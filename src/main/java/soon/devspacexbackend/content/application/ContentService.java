6f9a116027d55640233675a0742874477b4e1dc1
soonhankwon <soonable@gmail.com>
2023-07-21 11:47:35 +0900

    feat : 전체 컨텐츠 조회, 상세 컨텐츠 조회 추상화

--- src/main/java/soon/devspacexbackend/content/application/ContentService.java
+++ src/main/java/soon/devspacexbackend/content/application/ContentService.java
@@ -1,8 +1,17 @@
 package soon.devspacexbackend.content.application;
 
+import org.springframework.data.domain.Pageable;
+import soon.devspacexbackend.content.presentation.dto.ContentGetResDto;
 import soon.devspacexbackend.content.presentation.dto.ContentRegisterReqDto;
+import soon.devspacexbackend.user.domain.User;
+
+import java.util.List;
 
 public interface ContentService {
 
-    void registerContent(ContentRegisterReqDto dto);
+    void registerContent(ContentRegisterReqDto dto, User loginUser);
+
+    List<ContentGetResDto> getAllContent(Pageable pageable);
+
+    ContentGetResDto getContent(Long contentId, User loginUser);
 }
