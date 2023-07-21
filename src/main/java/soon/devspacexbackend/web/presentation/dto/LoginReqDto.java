5f07a2e9990443cc19ff3110e5ec5036bbbc3a94
soonhankwon <soonable@gmail.com>
2023-07-21 11:50:36 +0900

    feat : DTO Swagger 설명 추가

--- src/main/java/soon/devspacexbackend/web/presentation/dto/LoginReqDto.java
+++ src/main/java/soon/devspacexbackend/web/presentation/dto/LoginReqDto.java
@@ -12,8 +12,10 @@ import javax.validation.constraints.NotEmpty;
 public final class LoginReqDto {
 
     @NotEmpty
+    @Schema(description = "유저 이메일", example = "dev@space.com")
     private final String email;
 
     @NotEmpty
+    @Schema(description = "유저 패스워드", example = "1234")
     private final String password;
 }
