98dc919885ad2d3d31de348b1d7bf91c060c91ac
soonhankwon <soonable@gmail.com>
2023-07-21 11:47:15 +0900

    feat : 전체 컨텐츠 조회 + 페이지네이션 쿼리

--- src/main/java/soon/devspacexbackend/content/infrastructure/persistence/ContentRepository.java
+++ src/main/java/soon/devspacexbackend/content/infrastructure/persistence/ContentRepository.java
@@ -1,9 +1,13 @@
 package soon.devspacexbackend.content.infrastructure.persistence;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import soon.devspacexbackend.content.domain.Content;
 
 @Repository
 public interface ContentRepository extends JpaRepository<Content, Long> {
+
+    Page<Content> findAll(Pageable pageable);
 }
