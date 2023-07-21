74750411b013ef14ce56faa0816627fe9a2785a7
soonhankwon <soonable@gmail.com>
2023-07-21 11:40:13 +0900

    feat : CreatedTime entity Jpa auditing 사용

---
+++ src/main/java/soon/devspacexbackend/utils/CreatedTimeEntity.java
@@ -0,0 +1,18 @@
+package soon.devspacexbackend.utils;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@EntityListeners(value = {AuditingEntityListener.class})
+@MappedSuperclass
+public abstract class CreatedTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
