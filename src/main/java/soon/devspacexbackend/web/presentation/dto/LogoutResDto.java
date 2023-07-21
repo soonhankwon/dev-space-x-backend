6b04b3ed6b43b71aa7558b96f0679af47c5a58e1
soonhankwon <soonable@gmail.com>
2023-07-21 11:50:56 +0900

    feat : DTO Swagger 설명 추가

--- src/main/java/soon/devspacexbackend/web/presentation/dto/LogoutResDto.java
+++ src/main/java/soon/devspacexbackend/web/presentation/dto/LogoutResDto.java
@@ -9,5 +9,6 @@ import lombok.Getter;
 @Schema(description = "로그아웃 응답 DTO")
 public final class LogoutResDto {
 
+    @Schema(description = "로그아웃 성공 메세지", example = "로그아웃 성공")
     private final String message = "로그아웃 성공";
 }
