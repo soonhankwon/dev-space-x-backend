74ff33aa48c031a6259f207c60f5182da0239aed
soonhankwon <soonable@gmail.com>
2023-07-21 11:53:56 +0900

    feat : 유저 도메인 로직 추가 (결제로직)

--- src/main/java/soon/devspacexbackend/user/domain/User.java
+++ src/main/java/soon/devspacexbackend/user/domain/User.java
@@ -2,15 +2,20 @@ package soon.devspacexbackend.user.domain;
 
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import soon.devspacexbackend.content.domain.Content;
 import soon.devspacexbackend.user.presentation.dto.UserSignupReqDto;
+import soon.devspacexbackend.utils.CreatedTimeEntity;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
 @ToString
 @Table(name = "`user`")
-public class User {
+public class User extends CreatedTimeEntity {
+
+    private static final Long MIN_POINT = 0L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,15 +32,26 @@ public class User {
     @Enumerated(EnumType.STRING)
     private UserType userType;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserContent> userContents;
+
     public User(UserSignupReqDto dto) {
         this.email = dto.getEmail();
         this.name = dto.getName();
         this.password = dto.getPassword();
         this.userType = UserType.CANDIDATE;
-        this.darkMatter = 0L;
+        this.darkMatter = MIN_POINT;
     }
 
-    public boolean isPasswordValid(String password) {
-        return this.password.equals(password);
+    public void pay(Content content) {
+        Integer requiredDarkMatter = content.getDarkMatter();
+        if (this.darkMatter < requiredDarkMatter) {
+            throw new RuntimeException("다크매터가 부족합니다.");
+        }
+        this.darkMatter -= content.getDarkMatter();
+    }
+
+    public Long getId() {
+        return this.id;
     }
 }
