b6e56d385125185963fa45eb112e250d42130f23
soonhankwon <soonable@gmail.com>
2023-07-21 18:29:48 +0900

    feat : 유저 컨텐츠 리뷰 등록 응답 DTO

---
+++ src/main/java/soon/devspacexbackend/review/presentation/dto/ReviewRegisterResDto.java
@@ -0,0 +1,11 @@
+package soon.devspacexbackend.review.presentation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public final class ReviewRegisterResDto {
+
+    private final String message = "리뷰 등록 완료";
+}
