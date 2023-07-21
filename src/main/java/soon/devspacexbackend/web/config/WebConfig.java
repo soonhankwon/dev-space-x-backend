41106e9e3bec82b19fa195a7a8c5bc66a491cfd6
soonhankwon <soonable@gmail.com>
2023-07-21 11:56:18 +0900

    feat : 컨텐츠 인증, 인가관련 인터셉터 추가

--- src/main/java/soon/devspacexbackend/web/config/WebConfig.java
+++ src/main/java/soon/devspacexbackend/web/config/WebConfig.java
@@ -1,10 +1,11 @@
-package soon.devspacexbackend.config;
+package soon.devspacexbackend.web.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import soon.devspacexbackend.web.interceptor.LoginCheckInterceptor;
+import soon.devspacexbackend.web.interceptor.ContentAuthCheckInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -16,10 +17,14 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginCheckInterceptor())
+        registry.addInterceptor(new ContentAuthCheckInterceptor())
                 .order(1)
+                .addPathPatterns("/contents");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/", "/users/signup", "/login", "/logout",
-                        "/css/**","/js/**", "/*.ico", "/error", "/swagger-ui/**", "/v3/api-docs/**");
+                .excludePathPatterns("/", "/users/signup", "/contents", "/login", "/logout",
+                        "/css/**", "/js/**", "/*.ico", "/error", "/swagger-ui/**", "/v3/api-docs/**");
     }
 }
