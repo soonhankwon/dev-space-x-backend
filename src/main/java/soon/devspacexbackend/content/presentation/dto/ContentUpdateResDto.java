d25e7bcbee1eea163afa704c39049f41503c1131
soonhankwon <soonable@gmail.com>
2023-07-21 15:04:58 +0900

    feat : 컨텐츠 업데이트 응답 DTO

---
+++ src/main/java/soon/devspacexbackend/content/presentation/dto/ContentUpdateResDto.java
@@ -0,0 +1,11 @@
+package soon.devspacexbackend.content.presentation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public final class ContentUpdateResDto {
+
+    private final String message = "컨텐츠 업데이트 완료";
+}
