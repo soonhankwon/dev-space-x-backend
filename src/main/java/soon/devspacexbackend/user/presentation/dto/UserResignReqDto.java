fb904c8ff7eda024d5c27d74a1232bdcea6f67ff
soonhankwon <soonable@gmail.com>
2023-07-21 18:36:59 +0900

    feat : 회원삭제 요청 DTO

--- src/main/java/soon/devspacexbackend/user/presentation/dto/UserResignReqDto.java
+++ src/main/java/soon/devspacexbackend/user/presentation/dto/UserResignReqDto.java
@@ -3,12 +3,14 @@ package soon.devspacexbackend.user.presentation.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @Schema(description = "유저 회원삭제 요청 DTO")
 public final class UserResignReqDto {
 
     @Schema(description = "유저 패스워드", example = "1234")
-    private final String password;
+    private String password;
 }
