ada521e9a41d0c4a3b60726aa43395176ed00407
soonhankwon <soonable@gmail.com>
2023-07-21 11:46:42 +0900

    feat : DTO swagger 설명 추가

--- src/main/java/soon/devspacexbackend/content/presentation/dto/ContentRegisterReqDto.java
+++ src/main/java/soon/devspacexbackend/content/presentation/dto/ContentRegisterReqDto.java
@@ -1,18 +1,24 @@
 package soon.devspacexbackend.content.presentation.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import soon.devspacexbackend.content.domain.ContentPayType;
 
 @Getter
 @AllArgsConstructor
-public class ContentRegisterReqDto {
+@Schema(description = "컨텐츠 등록 요청 DTO")
+public final class ContentRegisterReqDto {
 
+    @Schema(description = "타이틀", example = "what is dev?")
     private final String title;
 
+    @Schema(description = "텍스트", example = "dev space x")
     private final String text;
 
+    @Schema(description = "컨텐츠 타입", example = "FREE")
     private final ContentPayType payType;
 
+    @Schema(description = "다크매터", example = "0")
     private final Integer darkMatter;
 }
