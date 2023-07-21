713b8991c60ae383336fc5f371edb44d4098ae64
soonhankwon <soonable@gmail.com>
2023-07-21 11:48:16 +0900

    feat : 전체 컨텐츠 조회, 상세 컨텐츠 조회 비즈니스 로직

--- src/main/java/soon/devspacexbackend/content/application/ContentServiceImpl.java
+++ src/main/java/soon/devspacexbackend/content/application/ContentServiceImpl.java
@@ -1,20 +1,67 @@
 package soon.devspacexbackend.content.application;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import soon.devspacexbackend.content.domain.Content;
+import soon.devspacexbackend.content.domain.ContentGetType;
 import soon.devspacexbackend.content.infrastructure.persistence.ContentRepository;
+import soon.devspacexbackend.content.presentation.dto.ContentGetResDto;
 import soon.devspacexbackend.content.presentation.dto.ContentRegisterReqDto;
+import soon.devspacexbackend.user.domain.BehaviorType;
+import soon.devspacexbackend.user.domain.User;
+import soon.devspacexbackend.user.domain.UserContent;
+import soon.devspacexbackend.user.infrastructure.persistence.UserContentRepository;
+import soon.devspacexbackend.user.infrastructure.persistence.UserRepository;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
 @RequiredArgsConstructor
 @Service
-public class ContentServiceImpl implements ContentService{
+public class ContentServiceImpl implements ContentService {
 
     private final ContentRepository contentRepository;
+    private final UserRepository userRepository;
+    private final UserContentRepository userContentRepository;
 
     @Override
-    public void registerContent(ContentRegisterReqDto dto) {
+    @Transactional
+    public void registerContent(ContentRegisterReqDto dto, User loginUser) {
         Content content = new Content(dto);
         contentRepository.save(content);
+        userContentRepository.save(new UserContent(loginUser, content, BehaviorType.POST));
+    }
+
+    @Override
+    public List<ContentGetResDto> getAllContent(Pageable pageable) {
+        Page<Content> contentPage = contentRepository.findAll(pageable);
+
+        return contentPage.getContent().stream()
+                .map(i -> i.convertContentGetResDto(ContentGetType.PREVIEW))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    @Transactional
+    public ContentGetResDto getContent(Long contentId, User loginUser) {
+        try {
+            Content content = contentRepository.findById(contentId)
+                    .orElseThrow(() -> new IllegalArgumentException("해당 컨텐츠를 찾을수 없습니다."));
+
+            User user = userRepository.findById(loginUser.getId())
+                    .orElseThrow(() -> new IllegalArgumentException("not exist user in db assertNot"));;
+            if(content.isTypePay()) {
+                user.pay(content);
+            }
+            return content.convertContentGetResDto(ContentGetType.VIEW);
+        } catch (IllegalArgumentException e) {
+            log.error("FrontPage Content ID or DB Data problem", e);
+            throw e;
+        }
     }
 }
