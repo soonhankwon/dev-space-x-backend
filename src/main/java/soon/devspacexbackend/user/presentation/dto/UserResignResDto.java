fd59551caab5455c7faa4f27024ee8fcf22525ee
soonhankwon <soonable@gmail.com>
2023-07-21 11:55:18 +0900

    feat : 회원 삭제 응답 DTO

---
+++ src/main/java/soon/devspacexbackend/user/presentation/dto/UserResignResDto.java
@@ -0,0 +1,14 @@
+package soon.devspacexbackend.user.presentation.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Schema(description = "유저 탈퇴 응답 DTO")
+public final class UserResignResDto {
+
+    @Schema(description = "탈퇴 성공 메세지", example = "회원탈퇴 완료")
+    private final String message = "회원탈퇴 완료";
+}
