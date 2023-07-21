f38eb0107912f9df30d9ddd3f0b69016b1c3d8f7
soonhankwon <soonable@gmail.com>
2023-07-21 18:34:51 +0900

    feat : 유저 컨텐츠 레포지토리 쿼리 추가

--- src/main/java/soon/devspacexbackend/user/infrastructure/persistence/UserContentRepository.java
+++ src/main/java/soon/devspacexbackend/user/infrastructure/persistence/UserContentRepository.java
@@ -7,11 +7,17 @@ import soon.devspacexbackend.user.domain.BehaviorType;
 import soon.devspacexbackend.user.domain.User;
 import soon.devspacexbackend.user.domain.UserContent;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserContentRepository extends JpaRepository<UserContent, Long> {
 
     boolean existsUserContentByContentAndUserAndType(Content content, User user, BehaviorType type);
+
     Optional<UserContent> findUserContentByContentAndUserAndType(Content content, User user, BehaviorType type);
+
+    List<UserContent> findUserContentsByContentAndType(Content content, BehaviorType type);
+
+    Optional<UserContent> findUserContentByContentAndType(Content content, BehaviorType type);
 }
