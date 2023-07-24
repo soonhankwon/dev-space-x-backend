ecdfa076ff13f58199124a852e9f47b1d8b8bee7
soonhankwon <soonable@gmail.com>
2023-07-24 14:15:27 +0900

    feat : 시리즈 서비스 구현 클래스 (비즈니스 로직)

---
+++ src/main/java/soon/devspacexbackend/series/application/SeriesServiceImpl.java
@@ -0,0 +1,91 @@
+package soon.devspacexbackend.series.application;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import soon.devspacexbackend.content.domain.Content;
+import soon.devspacexbackend.content.domain.ContentGetType;
+import soon.devspacexbackend.content.infrastructure.persistence.ContentRepository;
+import soon.devspacexbackend.content.presentation.dto.ContentGetResDto;
+import soon.devspacexbackend.content.presentation.dto.ContentRegisterReqDto;
+import soon.devspacexbackend.series.domain.Series;
+import soon.devspacexbackend.series.infrastructure.persistence.SeriesRepository;
+import soon.devspacexbackend.series.presentation.dto.SeriesGetResDto;
+import soon.devspacexbackend.series.presentation.dto.SeriesRegisterReqDto;
+import soon.devspacexbackend.series.presentation.dto.SeriesUpdateReqDto;
+import soon.devspacexbackend.user.domain.BehaviorType;
+import soon.devspacexbackend.user.domain.User;
+import soon.devspacexbackend.user.domain.UserContent;
+import soon.devspacexbackend.user.infrastructure.persistence.UserContentRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+class SeriesServiceImpl implements SeriesService {
+
+    private final SeriesRepository seriesRepository;
+    private final ContentRepository contentRepository;
+    private final UserContentRepository userContentRepository;
+
+    @Override
+    @Transactional
+    public void registerSeries(SeriesRegisterReqDto dto, User loginUser) {
+        if (seriesRepository.existsSeriesByName(dto.getSeriesName())) {
+            throw new IllegalArgumentException("중복된 시리즈 제목이 존재합니다.");
+        }
+        Series series = new Series(dto, loginUser);
+        seriesRepository.save(series);
+    }
+
+    @Override
+    @Transactional
+    public void registerSeriesContent(Long seriesId, ContentRegisterReqDto dto, User loginUser) {
+        Series series = seriesRepository.findById(seriesId)
+                .orElseThrow(() -> new IllegalArgumentException("not exists series"));
+        series.validateContentType(dto);
+
+        Content content = new Content(dto, series);
+        contentRepository.save(content);
+
+        userContentRepository.save(new UserContent(loginUser, content, BehaviorType.POST));
+    }
+
+    @Override
+    public List<SeriesGetResDto> getAllSeries(Pageable pageable) {
+        Page<Series> seriesPage = seriesRepository.findAll(pageable);
+
+        return seriesPage.stream()
+                .map(Series::convertSeriesGetResDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<ContentGetResDto> getSeriesContents(Long seriesId, Pageable pageable) {
+        Series series = seriesRepository.findById(seriesId)
+                .orElseThrow(() -> new IllegalArgumentException("not exist series"));
+
+        Page<Content> contentsPage = contentRepository.findAllBySeries(series, pageable);
+
+        return contentsPage.stream()
+                .map(i -> i.convertContentGetResDto(ContentGetType.PREVIEW))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    @Transactional
+    public void updateSeries(Long seriesId, SeriesUpdateReqDto dto, User loginUser) {
+        Series series = seriesRepository.findById(seriesId)
+                .orElseThrow(() -> new IllegalArgumentException("not exist series"));
+
+        if(seriesRepository.existsSeriesByName(dto.getName()))
+            throw new IllegalArgumentException("already exists series title");
+
+        series.update(dto, loginUser);
+    }
+}
