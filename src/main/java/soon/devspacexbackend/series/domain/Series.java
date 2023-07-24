50e0bb7ab7061c967626aa35e5d7f6bbb1b71724
soonhankwon <soonable@gmail.com>
2023-07-24 14:11:42 +0900

    feat : 시리즈 유료 또는 무료 타입 인스턴스 변수 추가 (enum), 시리즈 업데이트, 시리즈 컨텐츠타입 검증 로직

--- src/main/java/soon/devspacexbackend/series/domain/Series.java
+++ src/main/java/soon/devspacexbackend/series/domain/Series.java
@@ -1,6 +1,12 @@
 package soon.devspacexbackend.series.domain;
 
 import lombok.NoArgsConstructor;
+import soon.devspacexbackend.content.domain.ContentPayType;
+import soon.devspacexbackend.content.presentation.dto.ContentRegisterReqDto;
+import soon.devspacexbackend.series.presentation.dto.SeriesGetResDto;
+import soon.devspacexbackend.series.presentation.dto.SeriesRegisterReqDto;
+import soon.devspacexbackend.series.presentation.dto.SeriesUpdateReqDto;
+import soon.devspacexbackend.user.domain.User;
 
 import javax.persistence.*;
 
@@ -15,12 +21,51 @@ public class Series {
     private String name;
 
     @Enumerated(EnumType.STRING)
-    private SeriesType type;
+    private SeriesStatus status;
 
     @Enumerated(EnumType.STRING)
-    private SeriesStatus status;
+    private SeriesType type;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private User user;
+
+    public Series(SeriesRegisterReqDto dto, User loginUser) {
+        this.name = dto.getSeriesName();
+        this.status = SeriesStatus.SERIALIZED;
+        this.type = dto.getSeriesType();
+        this.user = loginUser;
+    }
 
     public String getName() {
         return name;
+    }
+
+    public SeriesGetResDto convertSeriesGetResDto() {
+        return new SeriesGetResDto(this.id, this.name, this.status, this.type, this.user.getName());
+    }
+
+    public void validateContentType(ContentRegisterReqDto dto) {
+        if (this.type == SeriesType.PAY) {
+            if (dto.getPayType() == ContentPayType.FREE)
+                throw new IllegalArgumentException("컨텐츠 타입은 유료여야 합니다.");
+        } else {
+            if (dto.getPayType() == ContentPayType.PAY)
+                throw new IllegalArgumentException("컨텐츠 타입은 무료여야 합니다.");
+        }
+    }
+
+    public void update(SeriesUpdateReqDto dto, User loginUser) {
+        validateSeriesUser(loginUser);
+        if(!dto.getName().isEmpty()) {
+            this.name = dto.getName();
+        }
+        this.status = dto.getStatus();
+        this.type = dto.getType();
+    }
+
+    private void validateSeriesUser(User loginUser) {
+        if(loginUser != this.user)
+            throw new IllegalArgumentException("no auth to update this series");
     }
 }
