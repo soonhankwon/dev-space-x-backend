0012bd97d468bef3dba04130cc667b7e35d01714
soonhankwon <soonable@gmail.com>
2023-07-24 14:08:58 +0900

    feat : 시리즈 모든 컨텐츠 찾기 (페이지네이션)

--- src/main/java/soon/devspacexbackend/content/infrastructure/persistence/ContentRepository.java
+++ src/main/java/soon/devspacexbackend/content/infrastructure/persistence/ContentRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import soon.devspacexbackend.content.domain.Content;
+import soon.devspacexbackend.series.domain.Series;
 
 @Repository
 public interface ContentRepository extends JpaRepository<Content, Long> {
 
     Page<Content> findAll(Pageable pageable);
+
+    Page<Content> findAllBySeries(Series series, Pageable pageable);
 }
