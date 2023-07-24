c5cd7d0f330632ea4f406921a7c9aa35a4829daa
soonhankwon <soonable@gmail.com>
2023-07-24 14:13:31 +0900

    feat : 시리즈 관련 서비스 인터페이스 (추상화)

---
+++ src/main/java/soon/devspacexbackend/series/application/SeriesService.java
@@ -0,0 +1,23 @@
+package soon.devspacexbackend.series.application;
+
+import org.springframework.data.domain.Pageable;
+import soon.devspacexbackend.content.presentation.dto.ContentGetResDto;
+import soon.devspacexbackend.content.presentation.dto.ContentRegisterReqDto;
+import soon.devspacexbackend.series.presentation.dto.SeriesGetResDto;
+import soon.devspacexbackend.series.presentation.dto.SeriesRegisterReqDto;
+import soon.devspacexbackend.series.presentation.dto.SeriesUpdateReqDto;
+import soon.devspacexbackend.user.domain.User;
+
+import java.util.List;
+
+public interface SeriesService {
+    void registerSeries(SeriesRegisterReqDto dto, User loginUser);
+
+    void registerSeriesContent(Long seriesId, ContentRegisterReqDto dto, User loginUser);
+
+    List<SeriesGetResDto> getAllSeries(Pageable pageable);
+
+    List<ContentGetResDto> getSeriesContents(Long seriesId, Pageable pageable);
+
+    void updateSeries(Long seriesId, SeriesUpdateReqDto dto, User loginUser);
+}
