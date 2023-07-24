728687be143e3d14ffc7ae1a7dce04dbf84ac826
soonhankwon <soonable@gmail.com>
2023-07-24 14:12:43 +0900

    feat : 시리즈 등록 요청 DTO

---
+++ src/main/java/soon/devspacexbackend/series/presentation/dto/SeriesRegisterReqDto.java
@@ -0,0 +1,18 @@
+package soon.devspacexbackend.series.presentation.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import soon.devspacexbackend.series.domain.SeriesType;
+
+@Getter
+@AllArgsConstructor
+@Schema(description = "시리즈 등록 요청 DTO")
+public final class SeriesRegisterReqDto {
+
+    @Schema(description = "시리즈 제목", example = "DEV SPACE X SERIES 1")
+    private final String seriesName;
+
+    @Schema(description = "시리즈 타입", example = "FREE")
+    private final SeriesType seriesType;
+}
