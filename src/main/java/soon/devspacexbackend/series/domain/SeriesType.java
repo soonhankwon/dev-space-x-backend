9a27f9ad939f949a34d71ad346b276aacb73a81f
soonhankwon <soonable@gmail.com>
2023-07-24 14:14:43 +0900

    feat : 시리즈 타입 무료, 유료

--- src/main/java/soon/devspacexbackend/series/domain/SeriesType.java
+++ src/main/java/soon/devspacexbackend/series/domain/SeriesType.java
@@ -1,8 +1,6 @@
 package soon.devspacexbackend.series.domain;
 
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
 public enum SeriesType {
-    SUN, MON, TUE, WED, THU, FRI, SAT, NO_LIMIT;
+
+    FREE, PAY
 }
