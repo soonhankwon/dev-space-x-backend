1c8ef441fe23c540e002468cd5aaf3bebe3dc7cc
soonhankwon <soonable@gmail.com>
2023-07-25 18:38:47 +0900

    feat : DTO 설명 추가

--- src/main/java/soon/devspacexbackend/review/presentation/dto/ReviewRegisterResDto.java
+++ src/main/java/soon/devspacexbackend/review/presentation/dto/ReviewRegisterResDto.java
@@ -1,11 +1,14 @@
 package soon.devspacexbackend.review.presentation.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Schema(description = "리뷰 등록 응답 DTO")
 public final class ReviewRegisterResDto {
 
+    @Schema(description = "리뷰 등록 성공 메세지", example = "리뷰 등록 완료")
     private final String message = "리뷰 등록 완료";
 }
