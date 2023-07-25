a1bb1b896fe416bd8528e4ff828f71c1e0f709c4
soonhankwon <soonable@gmail.com>
2023-07-25 18:31:15 +0900

    feat : 유저 다크메터 히스토리 조회 쿼리 (내림차순)

--- src/main/java/soon/devspacexbackend/darkmatter/infrastructure/persistence/DarkMatterHistoryRepository.java
+++ src/main/java/soon/devspacexbackend/darkmatter/infrastructure/persistence/DarkMatterHistoryRepository.java
@@ -3,7 +3,11 @@ package soon.devspacexbackend.darkmatter.infrastructure.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import soon.devspacexbackend.darkmatter.domain.DarkMatterHistory;
+import soon.devspacexbackend.user.domain.User;
+
+import java.util.List;
 
 @Repository
 public interface DarkMatterHistoryRepository extends JpaRepository<DarkMatterHistory, Long> {
+    List<DarkMatterHistory> findDarkMatterHistoriesByUserOrderByCreatedAtDesc(User user);
 }
