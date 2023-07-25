91b0f0587fd99d2ef1564d48487b1e178fe8ab60
soonhankwon <soonable@gmail.com>
2023-07-25 18:39:33 +0900

    feat : 리뷰 서비스 기능 추가 (추상화)

--- src/main/java/soon/devspacexbackend/review/application/ReviewService.java
+++ src/main/java/soon/devspacexbackend/review/application/ReviewService.java
@@ -1,8 +1,13 @@
 package soon.devspacexbackend.review.application;
 
 import soon.devspacexbackend.review.presentation.dto.ReviewRegisterReqDto;
+import soon.devspacexbackend.review.presentation.dto.ReviewUpdateReqDto;
 import soon.devspacexbackend.user.domain.User;
 
 public interface ReviewService {
     void registerReview(Long contentId, User loginUser, ReviewRegisterReqDto dto);
+
+    void updateReview(Long reviewId, User loginUser, ReviewUpdateReqDto dto);
+
+    void deleteReview(Long reviewId, User loginUser);
 }
