13bbf5639f1bb286bfe6d03eebd407a37f52b6dd
soonhankwon <soonable@gmail.com>
2023-07-25 18:33:52 +0900

    feat : 유저 컨텐츠 리뷰 수정 및 삭제 API

--- src/main/java/soon/devspacexbackend/review/presentation/ReviewController.java
+++ src/main/java/soon/devspacexbackend/review/presentation/ReviewController.java
@@ -6,8 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import soon.devspacexbackend.review.application.ReviewService;
-import soon.devspacexbackend.review.presentation.dto.ReviewRegisterReqDto;
-import soon.devspacexbackend.review.presentation.dto.ReviewRegisterResDto;
+import soon.devspacexbackend.review.presentation.dto.*;
 import soon.devspacexbackend.user.domain.User;
 import soon.devspacexbackend.web.application.SessionService;
 
@@ -29,5 +28,24 @@ public class ReviewController {
         User loginUser = sessionServiceImpl.getLoginUserBySession(request);
         reviewServiceIml.registerReview(contentId, loginUser, dto);
         return new ReviewRegisterResDto();
+    }
+
+    @PatchMapping("/{reviewId}")
+    @Operation(summary = "유저 컨텐츠 리뷰 수정 API")
+    @ResponseStatus(HttpStatus.OK)
+    public ReviewUpdateResDto updateReview(@PathVariable Long reviewId,
+                                           @RequestBody ReviewUpdateReqDto dto, HttpServletRequest request) {
+        User loginUser = sessionServiceImpl.getLoginUserBySession(request);
+        reviewServiceIml.updateReview(reviewId, loginUser, dto);
+        return new ReviewUpdateResDto();
+    }
+
+    @DeleteMapping("/{reviewId}")
+    @Operation(summary = "유저 컨텐츠 리뷰 삭제 API")
+    @ResponseStatus(HttpStatus.OK)
+    public ReviewDeleteResDto deleteReview(@PathVariable Long reviewId, HttpServletRequest request) {
+        User loginUser = sessionServiceImpl.getLoginUserBySession(request);
+        reviewServiceIml.deleteReview(reviewId, loginUser);
+        return new ReviewDeleteResDto();
     }
 }
