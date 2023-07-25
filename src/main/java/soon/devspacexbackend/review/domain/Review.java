6a4bc0c0e1db3a282e2a48d97df4a14d4232da59
soonhankwon <soonable@gmail.com>
2023-07-25 18:33:30 +0900

    feat : 리뷰 업데이트 로직, 리뷰 타입 체크 로직

--- src/main/java/soon/devspacexbackend/review/domain/Review.java
+++ src/main/java/soon/devspacexbackend/review/domain/Review.java
@@ -3,6 +3,7 @@ package soon.devspacexbackend.review.domain;
 import lombok.NoArgsConstructor;
 import soon.devspacexbackend.content.domain.Content;
 import soon.devspacexbackend.review.presentation.dto.ReviewRegisterReqDto;
+import soon.devspacexbackend.review.presentation.dto.ReviewUpdateReqDto;
 import soon.devspacexbackend.user.domain.User;
 import soon.devspacexbackend.utils.BaseTimeEntity;
 
@@ -29,11 +30,19 @@ public class Review extends BaseTimeEntity {
     @JoinColumn(name = "content_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private Content content;
 
-
     public Review(User user, Content content, ReviewRegisterReqDto dto) {
         this.type = dto.getType();
         this.comment = dto.getComment();
         this.user = user;
         this.content = content;
+    }
+
+    public void update(ReviewUpdateReqDto dto) {
+        this.type = dto.getType();
+        this.comment = dto.getComment();
+    }
+
+    public boolean isTypeLike() {
+        return this.type == ReviewType.LIKE;
     }
 }
