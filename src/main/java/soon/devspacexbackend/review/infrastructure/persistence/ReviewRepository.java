526e9e62953da164609ad420be7a7c2d1a4a90ae
soonhankwon <soonable@gmail.com>
2023-07-25 18:39:18 +0900

    feat : 리뷰 레포지토리 메서드 추가

--- src/main/java/soon/devspacexbackend/review/infrastructure/persistence/ReviewRepository.java
+++ src/main/java/soon/devspacexbackend/review/infrastructure/persistence/ReviewRepository.java
@@ -6,8 +6,14 @@ import soon.devspacexbackend.content.domain.Content;
 import soon.devspacexbackend.review.domain.Review;
 import soon.devspacexbackend.user.domain.User;
 
+import java.util.Optional;
+
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     boolean existsReviewByUserAndContent(User loginUser, Content content);
+
+    Optional<Review> findReviewByUser(User loginUser);
+
+    Optional<Review> findReviewByIdAndUser(Long id, User loginUser);
 }
