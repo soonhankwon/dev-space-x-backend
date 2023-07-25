a5a54586e3cab2d014307a18a048127832029732
soonhankwon <soonable@gmail.com>
2023-07-25 18:40:17 +0900

    feat : 리뷰 수정, 삭제 비즈니스로직 구현, 리뷰 작성시 자신의 컨텐츠의 리뷰를 하지못하도록 로직 추가

--- src/main/java/soon/devspacexbackend/review/application/ReviewServiceImpl.java
+++ src/main/java/soon/devspacexbackend/review/application/ReviewServiceImpl.java
@@ -6,10 +6,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import soon.devspacexbackend.content.domain.Content;
 import soon.devspacexbackend.content.infrastructure.persistence.ContentRepository;
+import soon.devspacexbackend.darkmatter.application.DarkMatterService;
 import soon.devspacexbackend.review.domain.Review;
+import soon.devspacexbackend.review.domain.ReviewType;
 import soon.devspacexbackend.review.infrastructure.persistence.ReviewRepository;
 import soon.devspacexbackend.review.presentation.dto.ReviewRegisterReqDto;
+import soon.devspacexbackend.review.presentation.dto.ReviewUpdateReqDto;
+import soon.devspacexbackend.user.domain.BehaviorType;
 import soon.devspacexbackend.user.domain.User;
+import soon.devspacexbackend.user.infrastructure.persistence.UserContentRepository;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -19,6 +24,8 @@ public class ReviewServiceImpl implements ReviewService {
 
     private final ReviewRepository reviewRepository;
     private final ContentRepository contentRepository;
+    private final DarkMatterService darkMatterServiceImpl;
+    private final UserContentRepository userContentRepository;
 
     @Override
     @Transactional
@@ -26,11 +33,39 @@ public class ReviewServiceImpl implements ReviewService {
         Content content = contentRepository.findById(contentId)
                 .orElseThrow(() -> new IllegalArgumentException("not exist content"));
 
+        if(userContentRepository.existsUserContentByContentAndUserAndType(content, loginUser, BehaviorType.POST)) {
+            throw new IllegalArgumentException("can't review myself");
+        }
+
         if(reviewRepository.existsReviewByUserAndContent(loginUser, content)) {
             throw new IllegalArgumentException("already exist review");
         }
 
         Review review = new Review(loginUser, content, dto);
         reviewRepository.save(review);
+        darkMatterServiceImpl.changeUserDarkMatterByReview(loginUser, review);
+    }
+
+    @Override
+    @Transactional
+    public void updateReview(Long reviewId, User loginUser, ReviewUpdateReqDto dto) {
+        Review review = reviewRepository.findReviewByUser(loginUser)
+                .orElseThrow(() -> new IllegalArgumentException("not exist review"));
+
+        if(!review.isTypeLike() && dto.getType() == ReviewType.LIKE) {
+            darkMatterServiceImpl.changeUserDarkMatterByReview(loginUser, review);
+            review.update(dto);
+        } else {
+            review.update(dto);
+        }
+    }
+
+    @Override
+    @Transactional
+    public void deleteReview(Long reviewId, User loginUser) {
+        Review review = reviewRepository.findReviewByIdAndUser(reviewId, loginUser)
+                .orElseThrow(() -> new IllegalArgumentException("not exist review by user"));
+
+        reviewRepository.delete(review);
     }
 }
