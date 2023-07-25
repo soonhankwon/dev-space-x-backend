abd9a6915f9f475b956841841f34b103a73ea9ef
soonhankwon <soonable@gmail.com>
2023-07-25 18:38:30 +0900

    feat : DTO 설명 추가

--- src/main/java/soon/devspacexbackend/content/presentation/dto/ContentUpdateResDto.java
+++ src/main/java/soon/devspacexbackend/content/presentation/dto/ContentUpdateResDto.java
@@ -1,11 +1,14 @@
 package soon.devspacexbackend.content.presentation.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Schema(description = "컨텐츠 업데이트 응답 DTO")
 public final class ContentUpdateResDto {
 
+    @Schema(description = "컨텐츠 업데이트 성공 메세지", example = "컨텐츠 업데이트 완료")
     private final String message = "컨텐츠 업데이트 완료";
 }
