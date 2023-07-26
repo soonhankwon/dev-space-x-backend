18f4709bacd4668512fa451a34f42d9271dde3b8
soonhankwon <soonable@gmail.com>
2023-07-26 19:19:41 +0900

    feat : ApiException handle, @Validated 검증시 MethodArgEx handle

--- src/main/java/soon/devspacexbackend/exception/ExControllerAdvice.java
+++ src/main/java/soon/devspacexbackend/exception/ExControllerAdvice.java
@@ -3,12 +3,26 @@ package soon.devspacexbackend.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.HttpSessionRequiredException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExControllerAdvice {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler
+    public ErrorResult apiExHandle(ApiException e) {
+        return new ErrorResult(HttpStatus.BAD_REQUEST, e.getCustomErrorCode().getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler
+    public ErrorResult methodArgNotValidExHandle(MethodArgumentNotValidException e) {
+        return new ErrorResult(HttpStatus.BAD_REQUEST,
+                e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+    }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler
