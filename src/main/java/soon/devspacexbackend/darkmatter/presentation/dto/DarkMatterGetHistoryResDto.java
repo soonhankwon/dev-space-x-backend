478734f102634325f703405b9e24b67bb5993cfd
soonhankwon <soonable@gmail.com>
2023-07-27 19:22:56 +0900

    feat : 유저 다크매터 이력 조회 응답 DTO 설명 추가

--- src/main/java/soon/devspacexbackend/darkmatter/presentation/dto/DarkMatterGetHistoryResDto.java
+++ src/main/java/soon/devspacexbackend/darkmatter/presentation/dto/DarkMatterGetHistoryResDto.java
@@ -9,12 +9,15 @@ import java.time.LocalDateTime;
 
 @Getter
 @AllArgsConstructor
-@Schema(description = "유저 다크메터 이력 조회 응답 DTO")
+@Schema(description = "유저 다크매터 이력 조회 응답 DTO")
 public final class DarkMatterGetHistoryResDto {
 
+    @Schema(description = "이력 변동 타입", example = "CHARGE")
     private final ChangeType type;
 
+    @Schema(description = "변동 다크매터", example = "100")
     private final Long darkMatter;
 
+    @Schema(description = "이력 변동 시간", example = "2023-07-12T17:56:26.643536")
     private final LocalDateTime createAt;
 }
