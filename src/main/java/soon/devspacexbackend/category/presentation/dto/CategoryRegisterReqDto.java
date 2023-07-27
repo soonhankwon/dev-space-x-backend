7d3ca7acbe7e459a732ae6f28985c706fcaff74d
soonhankwon <soonable@gmail.com>
2023-07-27 19:38:31 +0900

    feat : 카테고리 이름 검증 추가 NotBlank

--- src/main/java/soon/devspacexbackend/category/presentation/dto/CategoryRegisterReqDto.java
+++ src/main/java/soon/devspacexbackend/category/presentation/dto/CategoryRegisterReqDto.java
@@ -5,12 +5,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Schema(description = "카테고리 등록 요청 DTO")
 public final class CategoryRegisterReqDto {
 
+    @NotBlank(message = "카테고리 이름은 하나 이상의 공백이 아닌 문자를 포함해야 합니다.")
     @Schema(description = "카테고리 이름", example = "JAVA")
     private String name;
 }
