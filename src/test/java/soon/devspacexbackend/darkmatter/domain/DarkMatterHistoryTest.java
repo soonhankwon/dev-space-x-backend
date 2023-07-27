5743faa2544112d18b3b17bd3e3e2e00091c122b
soonhankwon <soonable@gmail.com>
2023-07-27 19:26:41 +0900

    test : 유저 다크매터 이력 조회 응답 DTO 로 변환 테스트

--- src/test/java/soon/devspacexbackend/darkmatter/domain/DarkMatterHistoryTest.java
+++ src/test/java/soon/devspacexbackend/darkmatter/domain/DarkMatterHistoryTest.java
@@ -2,6 +2,7 @@ package soon.devspacexbackend.darkmatter.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import soon.devspacexbackend.darkmatter.presentation.dto.DarkMatterGetHistoryResDto;
 import soon.devspacexbackend.user.domain.User;
 import soon.devspacexbackend.user.presentation.dto.UserSignupReqDto;
 
@@ -14,7 +15,6 @@ class DarkMatterHistoryTest {
     void createDarkMatterHistory() {
         UserSignupReqDto dto1 = new UserSignupReqDto(
                 "test@gmail.com", "tester", "1234");
-
         User user1 = new User(dto1);
         DarkMatterHistory darkMatterHistory = new DarkMatterHistory(user1, ChangeType.CHARGE, 1);
 
@@ -22,6 +22,16 @@ class DarkMatterHistoryTest {
     }
 
     @Test
+    @DisplayName("유저 다크매터 이력 조회 응답 DTO 로 변환 테스트")
     void convertDarkMatterGetHistoryResDto() {
+        UserSignupReqDto dto1 = new UserSignupReqDto(
+                "test@gmail.com", "tester", "1234");
+        User user1 = new User(dto1);
+        DarkMatterHistory darkMatterHistory = new DarkMatterHistory(user1, ChangeType.CHARGE, 1);
+
+        DarkMatterGetHistoryResDto res = darkMatterHistory.convertDarkMatterGetHistoryResDto();
+
+        assertThat(res.getDarkMatter()).isEqualTo(1);
+        assertThat(res.getType()).isEqualTo(ChangeType.CHARGE);
     }
 }
