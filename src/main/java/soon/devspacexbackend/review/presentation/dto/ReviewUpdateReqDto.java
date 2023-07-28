6071271aa3fa8d76cd863ac3c482e3359366f0d5
soonhankwon <soonable@gmail.com>
2023-07-28 16:09:15 +0900

    feat : 리뷰 수정 요청 DTO Validated 적용

--- src/main/java/soon/devspacexbackend/review/presentation/dto/ReviewUpdateReqDto.java
+++ src/main/java/soon/devspacexbackend/review/presentation/dto/ReviewUpdateReqDto.java
@@ -5,14 +5,21 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import soon.devspacexbackend.review.domain.ReviewType;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @AllArgsConstructor
 @Getter
 @Schema(description = "유저 컨텐츠 리뷰 수정 요청 DTO")
 public final class ReviewUpdateReqDto {
 
+    @NotNull(message = "리뷰 타입은 null 일 수 없습니다.")
     @Schema(description = "좋아요 또는 아쉬워요", example = "LIKE")
     private final ReviewType type;
 
+    @NotBlank(message = "리뷰내용은 하나 이상의 공백이 아닌 문자를 포함해야 합니다.")
+    @Size(min = 1, max = 200, message = "댓글은 1~200 글자만 허용됩니다.")
     @Schema(description = "댓글", example = "생각해보니 너무 유익한 내용입니다.")
     private final String comment;
 }
