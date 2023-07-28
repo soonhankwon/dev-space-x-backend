bdbb3560469d72c54545d81ae485d0793665ab97
soonhankwon <soonable@gmail.com>
2023-07-28 16:02:44 +0900

    chore : 오타 수정

--- src/main/java/soon/devspacexbackend/category/presentation/dto/CategoryGetResDto.java
+++ src/main/java/soon/devspacexbackend/category/presentation/dto/CategoryGetResDto.java
@@ -9,9 +9,9 @@ import lombok.Getter;
 @Schema(description = "카테고리 조회 응답 DTO")
 public final class CategoryGetResDto {
 
-    @Schema(description = "카테소리 ID", example = "1")
+    @Schema(description = "카테고리 ID", example = "1")
     private Long id;
 
-    @Schema(description = "카테소리 이름", example = "JAVA")
+    @Schema(description = "카테고리 이름", example = "JAVA")
     private String name;
 }
