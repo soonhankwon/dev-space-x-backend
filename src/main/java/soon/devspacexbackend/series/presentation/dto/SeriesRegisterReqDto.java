29ca689674bbcdacce13fefa195659fafa3adcf8
soonhankwon <soonable@gmail.com>
2023-07-28 18:43:56 +0900

    fix : Jackson binding error 로 NoArgs 추가

--- src/main/java/soon/devspacexbackend/series/presentation/dto/SeriesRegisterReqDto.java
+++ src/main/java/soon/devspacexbackend/series/presentation/dto/SeriesRegisterReqDto.java
@@ -2,8 +2,8 @@ package soon.devspacexbackend.series.presentation.dto;
 
 import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import soon.devspacexbackend.category.domain.Category;
 import soon.devspacexbackend.series.domain.SeriesType;
 
@@ -13,26 +13,33 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 @Schema(description = "시리즈 등록 요청 DTO")
 public final class SeriesRegisterReqDto {
 
     @NotBlank(message = "시리즈 타이틀은 하나 이상의 공백이 아닌 문자를 포함해야 합니다.")
     @Size(min = 1, max = 50, message = "시리즈 타이틀은 1~50 글자만 허용됩니다.")
     @Schema(description = "시리즈 제목", example = "DEV SPACE X SERIES 1")
-    private final String seriesName;
+    private String seriesName;
 
     @NotNull(message = "시리즈 타입은 null 일 수 없습니다.")
     @Schema(description = "시리즈 타입", example = "FREE")
-    private final SeriesType seriesType;
+    private SeriesType seriesType;
 
     @NotNull(message = "시리즈의 카테고리 ID는 null 일 수 없습니다.")
     @Min(1)
     @Schema(description = "카테고리 ID", example = "1")
-    private final Long categoryId;
+    private Long categoryId;
 
     @Hidden
-    private final Category category;
+    private Category category;
+
+    public SeriesRegisterReqDto(String seriesName, SeriesType seriesType, Long categoryId) {
+        this.seriesName = seriesName;
+        this.seriesType = seriesType;
+        this.categoryId = categoryId;
+        this.category = null;
+    }
 
     public SeriesRegisterReqDto(SeriesRegisterReqDto dto, Category category) {
         this.seriesName = dto.getSeriesName();
