1d0d8ff5b683d21f7b9b0ad8b21361e9332ee2b2
soonhankwon <soonable@gmail.com>
2023-07-28 18:46:53 +0900

    feat : 유저 타입에 업그레이드 여부 검증을 위한 필드 추가, 경험치에 맞는 유저타입 반환 정적팩토리 메서드 추가

--- src/main/java/soon/devspacexbackend/user/domain/UserType.java
+++ src/main/java/soon/devspacexbackend/user/domain/UserType.java
@@ -1,16 +1,36 @@
 package soon.devspacexbackend.user.domain;
 
+import lombok.Getter;
+
+@Getter
 public enum UserType {
-    CANDIDATE(0L),
-    PILOT(50L),
-    SECOND_PILOT(100L),
-    FIRST_PILOT(200L),
-    COMMANDER(500L),
-    ADMIN(-1234L);
+    CANDIDATE(0L, 50L),
+    PILOT(50L, 100L),
+    SECOND_PILOT(100L, 200L),
+    FIRST_PILOT(200L, 500L),
+    COMMANDER(500L, Long.MAX_VALUE),
+    ADMIN(-1234L, -1234L);
 
-    private Long requiredLike;
+    private final Long requiredExp;
+    private final Long nextRequiredExp;
 
-    UserType(Long requiredLike) {
-        this.requiredLike = requiredLike;
+    UserType(Long requiredExp, Long nextRequiredExp) {
+        this.requiredExp = requiredExp;
+        this.nextRequiredExp = nextRequiredExp;
+    }
+
+    public static UserType ofAvailableUpgradeType(Long exp) {
+        if(exp >= PILOT.requiredExp && exp < SECOND_PILOT.requiredExp) {
+            return PILOT;
+        }
+        else if (exp >= SECOND_PILOT.requiredExp && exp < FIRST_PILOT.requiredExp) {
+            return SECOND_PILOT;
+        }
+        else if (exp >= FIRST_PILOT.requiredExp && exp < COMMANDER.requiredExp) {
+            return FIRST_PILOT;
+        }
+        else {
+            return COMMANDER;
+        }
     }
 }
