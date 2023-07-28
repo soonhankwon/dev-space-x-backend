8e90a5c6f59754b5ddc480090d435a47caf6312a
soonhankwon <soonable@gmail.com>
2023-07-28 20:02:31 +0900

    feat : 커스텀 컨텐츠 레포지토리 custom query 구현 - querydsl

---
+++ src/main/java/soon/devspacexbackend/content/infrastructure/persistence/ContentRepositoryImpl.java
@@ -0,0 +1,33 @@
+package soon.devspacexbackend.content.infrastructure.persistence;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import soon.devspacexbackend.content.domain.Content;
+import soon.devspacexbackend.content.domain.QContent;
+import soon.devspacexbackend.review.domain.QReview;
+import soon.devspacexbackend.review.domain.ReviewType;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Repository
+public class ContentRepositoryImpl implements ContentRepositoryCustom{
+
+    private final JPAQueryFactory queryFactory;
+
+    QReview review = QReview.review;
+    QContent content = QContent.content;
+
+    @Override
+    public List<Content> findTop3ContentsByReviewType(ReviewType type) {
+        return queryFactory.select(content)
+                .from(content)
+                .join(review).on(content.id.eq(review.content.id))
+                .where(review.type.eq(type))
+                .groupBy(content)
+                .orderBy(review.count().desc())
+                .limit(3)
+                .fetch();
+    }
+}
