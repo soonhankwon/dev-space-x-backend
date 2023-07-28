5ce8f90829fcd4a707ad5f84b941ffb231127c1c
soonhankwon <soonable@gmail.com>
2023-07-28 16:02:21 +0900

    feat : 관리자용 API 예외처리

--- src/main/java/soon/devspacexbackend/category/presentation/CategoryController.java
+++ src/main/java/soon/devspacexbackend/category/presentation/CategoryController.java
@@ -9,6 +9,8 @@ import soon.devspacexbackend.category.application.CategoryService;
 import soon.devspacexbackend.category.presentation.dto.CategoryGetResDto;
 import soon.devspacexbackend.category.presentation.dto.CategoryRegisterReqDto;
 import soon.devspacexbackend.category.presentation.dto.CategoryRegisterResDto;
+import soon.devspacexbackend.user.domain.User;
+import soon.devspacexbackend.web.application.SessionService;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -16,22 +18,27 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/category")
-@Tag(name = "카테고리 관련 API - 관리자용")
+@Tag(name = "카테고리 관련 API")
 public class CategoryController {
 
     private final CategoryService categoryServiceImpl;
+    private final SessionService sessionServiceImpl;
 
     @PostMapping
-    @Operation(summary = "카테고리 등록 API")
+    @Operation(summary = "카테고리 등록 API - 관리자용")
     @ResponseStatus(HttpStatus.CREATED)
     public CategoryRegisterResDto registerCategory(@RequestBody CategoryRegisterReqDto dto, HttpServletRequest request) {
+        User loginUser = sessionServiceImpl.getLoginUserBySession(request);
+        if(!loginUser.isTypeAdmin()) {
+            throw new IllegalStateException("no auth to access api");
+        }
         categoryServiceImpl.registerCategory(dto);
         return new CategoryRegisterResDto();
     }
 
     @GetMapping
     @Operation(summary = "전체 카테고리 조회 API")
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.OK)
     public List<CategoryGetResDto> getAllCategories() {
         return categoryServiceImpl.getAllCategories();
     }
