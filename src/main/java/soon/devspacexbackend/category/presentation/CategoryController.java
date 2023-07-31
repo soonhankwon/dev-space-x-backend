19a05817a189dcc2eb79c3c937fba014a7022a01
soonhankwon <soonable@gmail.com>
2023-07-31 11:11:44 +0900

    feat : ApiResponse 사용 & ResDto 에 단일 메세지 필드 클래스들 -> GlobalResDto 로 대체

--- src/main/java/soon/devspacexbackend/category/presentation/CategoryController.java
+++ src/main/java/soon/devspacexbackend/category/presentation/CategoryController.java
@@ -1,6 +1,7 @@
 package soon.devspacexbackend.category.presentation;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -8,9 +9,9 @@ import org.springframework.web.bind.annotation.*;
 import soon.devspacexbackend.category.application.CategoryService;
 import soon.devspacexbackend.category.presentation.dto.CategoryGetResDto;
 import soon.devspacexbackend.category.presentation.dto.CategoryRegisterReqDto;
-import soon.devspacexbackend.category.presentation.dto.CategoryRegisterResDto;
 import soon.devspacexbackend.user.domain.User;
 import soon.devspacexbackend.web.application.SessionService;
+import soon.devspacexbackend.web.presentation.dto.GlobalResDto;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -26,14 +27,15 @@ public class CategoryController {
 
     @PostMapping
     @Operation(summary = "카테고리 등록 API - 관리자용")
+    @ApiResponse(responseCode = "201", description = "카테고리 등록 완료")
     @ResponseStatus(HttpStatus.CREATED)
-    public CategoryRegisterResDto registerCategory(@RequestBody CategoryRegisterReqDto dto, HttpServletRequest request) {
+    public GlobalResDto registerCategory(@RequestBody CategoryRegisterReqDto dto, HttpServletRequest request) {
         User loginUser = sessionServiceImpl.getLoginUserBySession(request);
         if(!loginUser.isTypeAdmin()) {
             throw new IllegalStateException("no auth to access api");
         }
         categoryServiceImpl.registerCategory(dto);
-        return new CategoryRegisterResDto();
+        return new GlobalResDto("카테고리 등록 완료");
     }
 
     @GetMapping
