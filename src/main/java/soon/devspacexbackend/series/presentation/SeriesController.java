64025efa7b293fecd4cabebfb35b3607fac497cb
soonhankwon <soonable@gmail.com>
2023-07-31 11:19:35 +0900

    feat : ApiResponse 사용 & ResDto 에 단일 메세지 필드 클래스들 -> GlobalResDto 로 대체

--- src/main/java/soon/devspacexbackend/series/presentation/SeriesController.java
+++ src/main/java/soon/devspacexbackend/series/presentation/SeriesController.java
@@ -1,6 +1,7 @@
 package soon.devspacexbackend.series.presentation;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -9,11 +10,14 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import soon.devspacexbackend.content.presentation.dto.ContentGetResDto;
-import soon.devspacexbackend.content.presentation.dto.ContentRegisterResDto;
 import soon.devspacexbackend.series.application.SeriesService;
-import soon.devspacexbackend.series.presentation.dto.*;
+import soon.devspacexbackend.series.presentation.dto.SeriesContentRegisterReqDto;
+import soon.devspacexbackend.series.presentation.dto.SeriesGetResDto;
+import soon.devspacexbackend.series.presentation.dto.SeriesRegisterReqDto;
+import soon.devspacexbackend.series.presentation.dto.SeriesUpdateReqDto;
 import soon.devspacexbackend.user.domain.User;
 import soon.devspacexbackend.web.application.SessionService;
+import soon.devspacexbackend.web.presentation.dto.GlobalResDto;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -30,19 +34,21 @@ public class SeriesController {
     @PostMapping
     @Operation(summary = "시리즈 등록 API")
     @ResponseStatus(HttpStatus.CREATED)
-    public SeriesRegisterResDto registerSeries(@RequestBody SeriesRegisterReqDto dto, HttpServletRequest request) {
+    @ApiResponse(responseCode = "201", description = "시리즈 등록 완료")
+    public GlobalResDto registerSeries(@RequestBody SeriesRegisterReqDto dto, HttpServletRequest request) {
         User loginUser = sessionServiceImpl.getLoginUserBySession(request);
         seriesServiceImpl.registerSeries(dto, loginUser);
-        return new SeriesRegisterResDto();
+        return new GlobalResDto("시리즈 등록 완료");
     }
 
     @PostMapping("/{seriesId}")
     @Operation(summary = "시리즈 컨텐츠 등록 API")
     @ResponseStatus(HttpStatus.CREATED)
-    public ContentRegisterResDto registerSeriesContent(@PathVariable Long seriesId, @RequestBody SeriesContentRegisterReqDto dto, HttpServletRequest request) {
+    @ApiResponse(responseCode = "201", description = "시리즈 컨텐츠 등록 완료")
+    public GlobalResDto registerSeriesContent(@PathVariable Long seriesId, @RequestBody SeriesContentRegisterReqDto dto, HttpServletRequest request) {
         User loginUser = sessionServiceImpl.getLoginUserBySession(request);
         seriesServiceImpl.registerSeriesContent(seriesId, dto, loginUser);
-        return new ContentRegisterResDto();
+        return new GlobalResDto("시리즈 컨텐츠 등록 완료");
     }
 
     @GetMapping
@@ -62,9 +68,20 @@ public class SeriesController {
     @PatchMapping("/{seriesId}")
     @Operation(summary = "시리즈 업데이트 API")
     @ResponseStatus(HttpStatus.OK)
-    public SeriesUpdateResDto updateSeries(@PathVariable Long seriesId, @RequestBody SeriesUpdateReqDto dto, HttpServletRequest request) {
+    @ApiResponse(responseCode = "200", description = "시리즈 수정 완료")
+    public GlobalResDto updateSeries(@PathVariable Long seriesId, @RequestBody SeriesUpdateReqDto dto, HttpServletRequest request) {
         User loginUser = sessionServiceImpl.getLoginUserBySession(request);
         seriesServiceImpl.updateSeries(seriesId, dto, loginUser);
-        return new SeriesUpdateResDto();
+        return new GlobalResDto("시리즈 수정 완료");
+    }
+
+    @DeleteMapping("/{seriesId}")
+    @Operation(summary = "시리즈 삭제 API")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiResponse(responseCode = "200", description = "시리즈 삭제 완료")
+    public GlobalResDto deleteSeries(@PathVariable Long seriesId, HttpServletRequest request) {
+        User loginUser = sessionServiceImpl.getLoginUserBySession(request);
+        seriesServiceImpl.deleteSeries(seriesId, loginUser);
+        return new GlobalResDto("시리즈 삭제 완료");
     }
 }
