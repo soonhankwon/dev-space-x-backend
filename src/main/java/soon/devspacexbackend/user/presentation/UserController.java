6c65e47a0f7f6615b5aa16df295a9ae447f0542f
soonhankwon <soonable@gmail.com>
2023-07-31 11:23:26 +0900

    feat : ApiResponse 사용 & ResDto 에 단일 메세지 필드 클래스들 -> GlobalResDto 로 대체

--- src/main/java/soon/devspacexbackend/user/presentation/UserController.java
+++ src/main/java/soon/devspacexbackend/user/presentation/UserController.java
@@ -1,17 +1,22 @@
 package soon.devspacexbackend.user.presentation;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import soon.devspacexbackend.content.presentation.dto.ContentGetResDto;
 import soon.devspacexbackend.exception.ApiException;
 import soon.devspacexbackend.exception.CustomErrorCode;
 import soon.devspacexbackend.user.application.UserService;
 import soon.devspacexbackend.user.domain.User;
-import soon.devspacexbackend.user.presentation.dto.*;
+import soon.devspacexbackend.user.presentation.dto.UserHistoryGetContentResDto;
+import soon.devspacexbackend.user.presentation.dto.UserResignReqDto;
+import soon.devspacexbackend.user.presentation.dto.UserSignupReqDto;
 import soon.devspacexbackend.web.application.SessionService;
+import soon.devspacexbackend.web.presentation.dto.GlobalResDto;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -29,19 +34,29 @@ public class UserController {
     @PostMapping("/signup")
     @Operation(summary = "회원 가입 API")
     @ResponseStatus(HttpStatus.CREATED)
-    public UserSignupResDto signupUser(@Validated @RequestBody UserSignupReqDto dto) {
+    @ApiResponse(responseCode = "201", description = "회원 가입 완료")
+    public GlobalResDto signupUser(@Validated @RequestBody UserSignupReqDto dto) {
         userServiceImpl.signupUser(dto);
-        return new UserSignupResDto();
+        return new GlobalResDto("회원 가입 완료");
     }
 
     @PostMapping("/resign")
     @Operation(summary = "회원 탈퇴 API")
     @ResponseStatus(HttpStatus.OK)
-    public UserResignResDto resignUser(@RequestBody UserResignReqDto dto, HttpServletRequest request) {
+    @ApiResponse(responseCode = "200", description = "회원 탈퇴 완료")
+    public GlobalResDto resignUser(@RequestBody UserResignReqDto dto, HttpServletRequest request) {
         User loginUser = sessionServiceImpl.getLoginUserBySession(request);
         userServiceImpl.resignUser(dto, loginUser);
         sessionServiceImpl.expireSession(request);
-        return new UserResignResDto();
+        return new GlobalResDto("회원 탈퇴 완료");
+    }
+
+    @GetMapping("/contents")
+    @Operation(summary = "회원이 가지고 있는 컨텐츠 조회 API : 포스팅한 컨텐츠 제외")
+    @ResponseStatus(HttpStatus.OK)
+    public List<ContentGetResDto> getUserContents(HttpServletRequest request) {
+        User loginUser = sessionServiceImpl.getLoginUserBySession(request);
+        return userServiceImpl.getUserContents(loginUser);
     }
 
     @GetMapping("/{contentId}/history")
