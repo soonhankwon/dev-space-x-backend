3a09238310eb85e904e23e529d609ebd0492a18b
soonhankwon <soonable@gmail.com>
2023-07-31 11:17:50 +0900

    feat : ApiResponse 사용 & ResDto 에 단일 메세지 필드 클래스들 -> GlobalResDto 로 대체

---
+++ src/main/java/soon/devspacexbackend/web/presentation/dto/GlobalResDto.java
@@ -0,0 +1,11 @@
+package soon.devspacexbackend.web.presentation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public final class GlobalResDto {
+
+    private final String message;
+}
