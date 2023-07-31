76e31bb71ac615d839dfa76bad4f3ef6b8716b06
soonhankwon <soonable@gmail.com>
2023-07-31 11:13:07 +0900

    feat : import querydsl config

--- src/test/java/soon/devspacexbackend/category/infrastructure/persistence/CategoryRepositoryTest.java
+++ src/test/java/soon/devspacexbackend/category/infrastructure/persistence/CategoryRepositoryTest.java
@@ -4,11 +4,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import soon.devspacexbackend.category.domain.Category;
 import soon.devspacexbackend.category.presentation.dto.CategoryRegisterReqDto;
+import soon.devspacexbackend.config.QuerydslConfig;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Import(QuerydslConfig.class)
 @DataJpaTest
 class CategoryRepositoryTest {
 
