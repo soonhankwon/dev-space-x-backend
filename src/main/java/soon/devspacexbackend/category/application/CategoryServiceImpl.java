f46fc1ee655edd4af79d9d04ad01d67b41b8b919
soonhankwon <soonable@gmail.com>
2023-08-03 12:27:28 +0900

    refactor : 카테고리 enum 클래스 사용 - 서비스 구현 코드 변경

--- src/main/java/soon/devspacexbackend/category/application/CategoryServiceImpl.java
+++ src/main/java/soon/devspacexbackend/category/application/CategoryServiceImpl.java
@@ -3,13 +3,10 @@ package soon.devspacexbackend.category.application;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import soon.devspacexbackend.category.domain.Category;
-import soon.devspacexbackend.category.infrastructure.persistence.CategoryRepository;
 import soon.devspacexbackend.category.presentation.dto.CategoryGetResDto;
-import soon.devspacexbackend.category.presentation.dto.CategoryRegisterReqDto;
-import soon.devspacexbackend.exception.CustomErrorCode;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,22 +15,10 @@ import java.util.stream.Collectors;
 @Service
 public class CategoryServiceImpl implements CategoryService{
 
-    private final CategoryRepository categoryRepository;
-
-    @Override
-    @Transactional
-    public void registerCategory(CategoryRegisterReqDto dto) {
-        if(categoryRepository.existsByName(dto.getName())) {
-            throw new IllegalArgumentException(CustomErrorCode.DUPLICATED_NAME.getMessage());
-        }
-        Category category = new Category(dto);
-        categoryRepository.save(category);
-    }
-
     @Override
     public List<CategoryGetResDto> getAllCategories() {
-        return categoryRepository.findAll().stream()
-                .map(Category::convertCategoryGetResDto)
+        return Arrays.stream(Category.values())
+                .map(CategoryGetResDto::ofDto)
                 .collect(Collectors.toList());
     }
 }
