4ebe592729f8c48493dc3500024e1debfbee2101
soonhankwon <soonable@gmail.com>
2023-08-03 12:46:15 +0900

    feat : email 중복여부 메서드 추가

--- src/main/java/soon/devspacexbackend/user/infrastructure/persistence/UserRepository.java
+++ src/main/java/soon/devspacexbackend/user/infrastructure/persistence/UserRepository.java
@@ -1,6 +1,7 @@
 package soon.devspacexbackend.user.infrastructure.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import soon.devspacexbackend.user.domain.User;
 
@@ -8,5 +9,9 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("select distinct u from User u left join fetch u.userContents where u.email = :email and u.password = :password")
     Optional<User> findUserByEmailAndPassword(String email, String password);
+
+    boolean existsUserByEmail(String email);
 }
