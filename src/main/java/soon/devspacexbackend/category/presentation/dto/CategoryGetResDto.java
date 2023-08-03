62e9d209b223b4a4f7956cbe93ea4d894f191f6b
soonhankwon <soonable@gmail.com>
2023-08-03 12:23:09 +0900

    feat : DTO 생성 정적팩토리 메서드

--- src/main/java/soon/devspacexbackend/category/presentation/dto/CategoryGetResDto.java
+++ src/main/java/soon/devspacexbackend/category/presentation/dto/CategoryGetResDto.java
@@ -3,15 +3,17 @@ package soon.devspacexbackend.category.presentation.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import soon.devspacexbackend.category.domain.Category;
 
 @Getter
 @AllArgsConstructor
 @Schema(description = "카테고리 조회 응답 DTO")
 public final class CategoryGetResDto {
 
-    @Schema(description = "카테고리 ID", example = "1")
-    private Long id;
-
     @Schema(description = "카테고리 이름", example = "JAVA")
     private String name;
+
+    public static CategoryGetResDto ofDto(Category category) {
+        return new CategoryGetResDto(category.name());
+    }
 }
